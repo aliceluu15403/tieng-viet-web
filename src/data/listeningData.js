8a0f5b8a68e61c18cc74f5cc97fa6ecd6cd3f531
Lưu Thu Hằng <luuthuhang@Luus-MacBook-Air.local>
2026-09-26 07:10:38 +0700

    Add listening MCQ mode and set MCQ as default view

--- src/data/listeningData.js
+++ src/data/listeningData.js
@@ -93,7 +93,7 @@ const allListeningLessons = {
       {
         id: 2,
         part1: "<b>A:</b> Gồm những",
-        part2: "ai vậy? &nbsp;&nbsp;|&nbsp;&nbsp; <b>B:</b> Ba mẹ, anh trai và em.",
+        part2: "vậy? &nbsp;&nbsp;|&nbsp;&nbsp; <b>B:</b> Ba mẹ, anh trai và em.",
         typeAnswer: "ai",
         correctMcq: "ai",
         mcqOptions: ["ai", "ái", "ải"]
